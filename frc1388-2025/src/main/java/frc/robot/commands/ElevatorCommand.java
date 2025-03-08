9e9a7263f0bdc30de369913a3252da56048e1585
Jeff Techau <jtechau@gmail.com>
2025-03-08 13:23:41 -0800

    Added one review comment

--- frc1388-2025/src/main/java/frc/robot/commands/ElevatorCommand.java
+++ frc1388-2025/src/main/java/frc/robot/commands/ElevatorCommand.java
@@ -92,7 +92,9 @@ public class ElevatorCommand extends Command {
 
   // Called once the command ends or is interrupted.
   @Override
-  public void end(boolean interrupted) {}
+  public void end(boolean interrupted) {
+//TODO: stop elevator motor?
+  }
 
   // Returns true when the command should end.
   @Override
