491b739d20dcec29c414704adffe843b1ab0b55b
Team1388 <programming@eaglerobotics.com>
2025-03-28 09:45:59 -0700

    fixed setpoints

--- frc1388-2025/src/main/java/frc/robot/subsystems/ElevatorSubsystem.java
+++ frc1388-2025/src/main/java/frc/robot/subsystems/ElevatorSubsystem.java
@@ -49,8 +49,8 @@ public class ElevatorSubsystem extends SubsystemBase {
     LEVEL3(26),
     LEVEL4(51.5),
 
-    ALGAEREMOVAL1(39.655), //algae betwen reef levels 2 and 3
-    ALGAEREMOVAL2(55.525); //algae between reef levels 3 and 4
+    ALGAEREMOVAL1(18.8), //algae betwen reef levels 2 and 3
+    ALGAEREMOVAL2(39); //algae between reef levels 3 and 4
     private double setpoint;
 
     private ElevatorSetPoints(double setpoint) {
