ee48b00cd17f0f65b6d1634296c4d8541edca68e
Team1388 <programming@eaglerobotics.com>
2025-03-27 17:30:47 -0700

    fix

--- frc1388-2025/src/main/java/frc/robot/subsystems/ElevatorSubsystem.java
+++ frc1388-2025/src/main/java/frc/robot/subsystems/ElevatorSubsystem.java
@@ -47,7 +47,7 @@ public class ElevatorSubsystem extends SubsystemBase {
     LEVEL1(0),
     LEVEL2(10),
     LEVEL3(26),
-    LEVEL4(51.5);
+    LEVEL4(51.5),
 
     ALGAEREMOVAL1(39.655), //algae betwen reef levels 2 and 3
     ALGAEREMOVAL2(55.525); //algae between reef levels 3 and 4
